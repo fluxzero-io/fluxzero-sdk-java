fd5c32fc76341ee453fe0b3aa16f3281e01973a0
renedewaele <renedewaele@users.noreply.github.com>
2025-11-06 23:38:49 +0000

    deploy: 81d638b36246e2ebea841d5f9476849a4dd66a6d

--- javadoc/proxy/apidocs/package-search-index.js
+++ javadoc/proxy/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.fluxzero.proxy"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.fluxzero.proxy"}];updateSearchResults();
--- javadoc/test-server/apidocs/package-search-index.js
+++ javadoc/test-server/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.fluxzero.testserver"},{"l":"io.fluxzero.testserver.metrics"},{"l":"io.fluxzero.testserver.scheduling"},{"l":"io.fluxzero.testserver.websocket"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.fluxzero.testserver"},{"l":"io.fluxzero.testserver.metrics"},{"l":"io.fluxzero.testserver.scheduling"},{"l":"io.fluxzero.testserver.websocket"}];updateSearchResults();
