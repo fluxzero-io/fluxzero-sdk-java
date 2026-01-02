c0d2f462a2636caaeec82e4218df328f5e03323f
René de Waele <renedewaele@gmail.com>
2026-01-02 16:31:01 +0100

    fix(test-fixture): update `andThen` method to return `Given<?>`
    This fixes tests in kotlin that use `andThen`.

--- sdk/src/test/java/io/fluxzero/sdk/test/Then.java
+++ sdk/src/test/java/io/fluxzero/sdk/test/Then.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Fluxzero IP B.V. or its affiliates. All Rights Reserved.
+ * Copyright (c) Fluxzero IP or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,6 +10,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package io.fluxzero.sdk.test;
@@ -941,6 +942,6 @@ public interface Then<R> {
      * <p>
      * This is useful for multi-step flows: e.g. {@code given(...).when(...).then(...).andThen().when(...).then(...)}.
      */
-    Given andThen();
+    Given<?> andThen();
 
 }
